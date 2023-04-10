f7bfd63ede21d8d01fa9005bd33a372c82a4c32d
Ruaridhi <ruaridhiscanlain@yahoo.co.uk>
2023-04-10 14:54:37 +0100

    Nav Header with routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import SearchForm from './SearchForm';
 import style from '@/styles/NavBar.module.css';
+import { useRouter } from 'next/router';
 
 export default function NavBar() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/');
+  };
+
   return (
     <div className={style.nav}>
+      <h3>
+        <a id={style.homeLink} onClick={handleClick}>City Weather</a>
+      </h3>
       <SearchForm />
     </div>
   );
